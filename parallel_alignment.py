e6cd18250c46a096d16c057a115f230ae8cfc99d
Trevor Gahl <tgahl7@gmail.com>
2017-11-20 17:11:16 -0700

    Added multithread capability

--- parallel_alignment.py
+++ parallel_alignment.py
@@ -1,12 +1,13 @@
 import threading
 #from array import array
 
-'''
+
 def worker(num):
-    print 'Worker: %s' % num
+    print '\nWorker: %s' % num
     return
 
 
+'''
 threads = []
 for i in range(5):
     t = threading.Thread(target=worker, args=(i,))
@@ -36,6 +37,18 @@ def createScoreMatrix(rows, cols):
     score_matrix = [[0 for col in range(cols)]for row in range(rows)]
     return score_matrix
 
+    for i in range(1, rows):
+        for j in range(1, cols):
+            similarity = match if seq1[i - 1] == seq2[j - 1] else other
+            diag_score = score_matrix[i - 1][j - 1] + similarity
+            up_score = score_matrix[i - 1][j] + other
+            left_score = score_matrix[i][j - 1] + other
+            curMax = max(0, diag_score, up_score, left_score)
+            if curMax > maxScore:
+                maxScore = curMax
+                maxPosition = (i, j)
+            score_matrix[i][j] = curMax
+
 
 rows = 4
 cols = 5
@@ -43,5 +56,16 @@ score_matrix = createScoreMatrix(rows, cols)
 # print score_matrix
 adiagonals = antidiagonals(score_matrix)
 # print len(adiagonals)
+threads = []
 for x in range(len(adiagonals)):
-    print len(adiagonals[x])
+    # print len(adiagonals[x])
+    for y in range(len(adiagonals[x])):
+        t = threading.Thread(target=worker, args=(y,))
+        threads.append(t)
+        t.start()
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=worker, args=(i,))
+    threads.append(t)
+    t.start()
